4eabca2bbf88ab7358d62f3cf1ddbf2a138034b2
zeineuub <ghada.malek12@gmail.com>
2020-04-07 08:12:13 +0000

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,7 @@ import { AuthService,GoogleLoginProvider} from 'angularx-social-login';
 })
 export class AppComponent {
   title = 'EasyUs';
-  user:any;
-  constructor(private socialAuthServ:AuthService){}
-//method to actually sign with google
-signIn(platform:string):void
-{
-  platform:GoogleLoginProvider.PROVIDER_ID;
-  this.socialAuthServ.signIn(platform)
-  .then(response=>{
-    console.log(platform + "logged i user dara is="+ response);
-    this.user=response;
 
-
-  }
-  );
-}
-//sign out method
-signOut():void
-{
-  this.socialAuthServ.signOut();
-  console.log("User Signed Out");
-}
 
 
 
