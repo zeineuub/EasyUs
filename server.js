49e73c9c2ad0b7943dd5657251b5b9b7f64c0dce
zeineuub <ghada.malek12@gmail.com>
2020-04-15 14:01:06 +0000

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
-app.use(express.static(_dirname +'/dist'));
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 //el port
 const PORT=process.env.Port||4200;
