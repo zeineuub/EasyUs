ad77010903d78979cadfa254e3067db7bac19619
zeineuub <ghada.malek12@gmail.com>
2020-04-15 18:28:23 +0000

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path=require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 //el port
